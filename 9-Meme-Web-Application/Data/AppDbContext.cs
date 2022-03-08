fc9383b8a1aa9b759fe6d12190672d9ef957cee7
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2022-03-08 11:39:34 +0200

    Add Class Libraries

---
+++ 9-Meme-Web-Application/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+	public class AppDbContext
+	{
+		public User User { get; set; }
+	}
+}
