6410098f7324e4ea374df2693867c43e1a679404
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2022-03-22 11:18:20 +0200

    Add Create Page

---
+++ 9-Meme-Web-Application/9-Meme-Web-Application/Controllers/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _9_Meme_Web_Application.Controllers
+{
+	public class PostsController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Post post)
+		{
+			// TODO: Add Service
+			return RedirectToAction(nameof(this.Index));
+		}
+	}
+}
